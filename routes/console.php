fe115e4f742f27fb366d9b26c2574e14b1e643ea
nermenibrahim <nermenibrahim051@gmail.com>
2024-08-27 20:53:07 +0300

    task15

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+//Schedule::command('app:send-email')->everyMinute();
+Schedule::command('app:expiration')->everyMinute();
+
+Schedule::command('backup:database')->daily();
+
+
+
+
+
+//schedule:بينفذ ليا تاسك معين كل فتره زمنيه معينه
+//command: حاجه بكتبها فى artisan cliبتنقذلى تاسك معين
